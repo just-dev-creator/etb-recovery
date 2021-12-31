dc004956bffe3cf0e031b1dff5b31942fce7c2f4
Justus <justus@justcoding.tech>
2021-12-31 15:30:49 +0100

    Created basic project & routing /edit/uuid routes to requestcomponent, everything else to homecomponent

--- web/src/app/app-routing.module.ts
+++ web/src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { RequestComponent } from "./request/request.component";
+import {HomeComponent} from "./home/home.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'edit/:id',
+    component: RequestComponent
+  },
+  {
+    path: '**',
+    component: HomeComponent
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
